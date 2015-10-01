55c6b1c724b8d29b269be24843f304e0999e2039
Frank Hofmann <frank.hofmann@efho.de>
2015-10-01 23:25:06 +0200

    assignments with calculations

--- assignments/assignments.rb
+++ assignments/assignments.rb
@@ -21,3 +21,13 @@ d,e = 2, 3						# set d to 2, and e to 3
 puts "a,b,c: #{a},#{b},#{c}"
 puts "d,e: #{d},#{e}"
 
+# assignments with calculations
+total = a + b - c + d - e		# simple addition
+output = colour * 4				# concatenate a string four times
+output2 = "light" + colour		# concatenate two strings
+output3 = colour + total.to_s	# concatenate a string with converted integer
+puts "total : #{total}"
+puts "colour: #{output}"
+puts "output2: #{output2}"
+puts "output3: #{output3}"
+
