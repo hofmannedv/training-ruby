8a9592e8c9287344cbbccc566b47920ea69bde2f
Frank Hofmann <frank.hofmann@efho.de>
2015-10-01 23:13:37 +0200

    multiple assignments

--- assignments/assignments.rb
+++ assignments/assignments.rb
@@ -14,3 +14,10 @@ length = 2.5					# float assignment
 puts "colour: #{colour}"
 puts "size  : #{size}"
 puts "length: #{length}"
+
+# multiple assignments
+a = b = c = 1					# set a, b, and c to 1
+d,e = 2, 3						# set d to 2, and e to 3
+puts "a,b,c: #{a},#{b},#{c}"
+puts "d,e: #{d},#{e}"
+
