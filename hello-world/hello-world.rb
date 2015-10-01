30aa9be137db0714ce6fe99dbcf4746daf2b1df4
Frank Hofmann <frank.hofmann@efho.de>
2015-10-01 18:12:22 +0200

    a ruby hello world program

---
+++ hello-world/hello-world.rb
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------
+# demonstrates a basic ruby program
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+puts "Hello, world!"
