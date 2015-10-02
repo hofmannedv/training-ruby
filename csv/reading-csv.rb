59c673b12e2a1865ce2acc326224c9c181965fba
Frank Hofmann <frank.hofmann@efho.de>
2015-10-02 16:13:14 +0200

    CSV: reading csv data

---
+++ csv/reading-csv.rb
@@ -0,0 +1,20 @@
+# -----------------------------------------------------------
+# reading data from a CSV file
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# import additional modules: csv
+require 'csv'
+
+# check the list of commandline arguments
+if ARGV.length < 1
+	puts "invalid number of parameters: 1 required."
+	puts "call: ruby reading-csv.rb database.txt"
+	puts "Exiting."
+else
+end
+
+
