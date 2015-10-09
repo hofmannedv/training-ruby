9f702a28f632822e9dbffaff07f0d0ced7dec945
Frank Hofmann <frank.hofmann@efho.de>
2015-10-09 19:20:16 +0200

    assignments: calculating the average of a list of integers

--- calculations/average.rb
+++ calculations/average.rb
@@ -7,24 +7,49 @@
 # -----------------------------------------------------------
 
 # define a list of values
-valueList = [1, 6, 3, 4, 5]
+$valueList = [1, 6, 3, 4, 5]
 
-# list length
-items = valueList.length
-
-# check for empty lists, first
-if items
-	# assume a total of 0
-	total = 0
+def sum1
+	total = 0.0
 
 	# go through the list element by element
-	valueList.each { |item| total+=item }
+	$valueList.each { |item| 
+		total+=item 
+	}
 
-	# calculate the average with explicit float conversion
-	average = total.to_f / items.to_f
-
-	# output minimum and maximum value
-	puts "The average value is: #{average}"
-else
-	puts "The list is empty."
+	return total
 end
+
+def sum2
+	total = 0.0
+
+	sum = $valueList.inject(:+).to_f
+
+	return sum
+end
+
+def sum3
+	total = 0.0
+
+	return $valueList.inject() { |sum, element| 
+		sum + element
+	}.to_f
+end
+
+def avg1
+	return $valueList.inject(0.0) { |avg, element| 
+		avg + (element.to_f / $valueList.size)
+	}
+end
+
+def avg2
+	avg = $valueList.inject(:+) / $valueList.size.to_f
+
+	return avg
+end
+
+puts "#{sum1()}"
+puts "#{sum2()}"
+puts "#{sum3()}"
+puts "#{avg1()}"
+puts "#{avg2()}"
