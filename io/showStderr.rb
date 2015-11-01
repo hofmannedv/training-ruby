b0ed2f9b6f240ae2ff2e33740ee78d9e8d185348
Frank Hofmann <frank.hofmann@efho.de>
2015-11-01 21:47:22 +0100

    io: output messages o STDOUT and STDERR

---
+++ io/showStderr.rb
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------
+# demonstrates error handling, and output via STDERR
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+
